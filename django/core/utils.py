ac22078154f82543996442d24fa4990ebb445805
gabrielucido <gabriel.rodrigues.0501@gmail.com>
2021-12-24 10:33:52 -0400

    Updated good-names.

--- django/core/utils.py
+++ django/core/utils.py
@@ -3,22 +3,22 @@ from rest_framework import exceptions
 from django.core.exceptions import ValidationError
 
 
-def prevent_not_found(p_k, model, queryset):
-    if p_k:
+def prevent_not_found(pk, model, queryset):
+    if pk:
         try:
-            model.objects.get(p_k=p_k)
+            model.objects.get(pk=pk)
         except model.DoesNotExist as error:
             raise exceptions.NotFound from error
         except ValidationError as error:
             raise exceptions.ValidationError from error
-        if not queryset.filter(p_k=p_k).exists():
+        if not queryset.filter(pk=pk).exists():
             raise exceptions.PermissionDenied
 
 
-def get_or_raise_error(p_k, model):
-    if p_k:
+def get_or_raise_error(pk, model):
+    if pk:
         try:
-            instance = model.objects.get(p_k=p_k)
+            instance = model.objects.get(pk=pk)
         except model.DoesNotExist as error:
             raise exceptions.NotFound from error
         except ValidationError as error:
