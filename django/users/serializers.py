a4881dd11350d0fe11e67a9ffa1ffae601c83c3a
Thyago Topan <30329564+topanthyago@users.noreply.github.com>
2022-01-21 13:01:38 -0300

    Create endpoint for Handle Auth for  Anon User (#18)

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-
 class UserSerializer(serializers.ModelSerializer):
     """
     User Serializer.
@@ -10,3 +9,22 @@ class UserSerializer(serializers.ModelSerializer):
         """User Serializer Options."""
         model = get_user_model()
         fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
+class CreateAnonUserSerializer(serializers.ModelSerializer):
+    """
+    Create anon user and return id and auth token.
+    """
+    token = serializers.CharField(read_only=True)
+    username = serializers.CharField(write_only=True)
+    class Meta:
+        """CreateAnonUserSerializer Options."""
+        model = get_user_model()
+        fields = ['username', 'token']
+        write_only_fields = ['username']
+    
+    def get_token(self, obj):
+        """
+        Get token and return it.
+        """
+        return obj.token.key
+        
