8088a4922047ad55c30da65f28970d295d565e1b
Thyago Topan <30329564+topanthyago@users.noreply.github.com>
2022-01-27 12:02:42 -0300

    Add Djoser and set endpoint's (#22)
    * Add Djoser and set endpoint's
    
    * Codequality.
    
    Co-authored-by: gabrielucido <gabrielucido@gmail.com>

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+
 class UserSerializer(serializers.ModelSerializer):
     """
     User Serializer.
     """
+
     class Meta:
         """User Serializer Options."""
         model = get_user_model()
         fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
 
 class CreateAnonUserSerializer(serializers.ModelSerializer):
     """
@@ -16,15 +19,16 @@ class CreateAnonUserSerializer(serializers.ModelSerializer):
     """
     token = serializers.CharField(read_only=True)
     username = serializers.CharField(write_only=True)
-    class Meta:
-        """CreateAnonUserSerializer Options."""
-        model = get_user_model()
-        fields = ['username', 'token']
-        write_only_fields = ['username']
-    
+
     def get_token(self, obj):
         """
         Get token and return it.
         """
         return obj.token.key
-        
+
+
+    class Meta:
+        """CreateAnonUserSerializer Options."""
+        model = get_user_model()
+        fields = ['username', 'token']
+        write_only_fields = ['username']
